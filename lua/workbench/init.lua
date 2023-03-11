220d1baeff945a68af3cff8a2a2d7cc9acd35932
Walcriz <walcriz.mail@gmail.com>
2023-03-11 10:05:09 +0100

    Respect new change to objects

--- lua/workbench/init.lua
+++ lua/workbench/init.lua
@@ -4,11 +4,11 @@ local branch_workbench = require("workbench.bench.branch_workbench")
 local workbench = {}
 
 workbench.toggle_branch_workbench = function()
-	branch_workbench.toggle()
+	branch_workbench:toggle()
 end
 
 workbench.toggle_project_workbench = function()
-	project_workbench.toggle()
+	project_workbench:toggle()
 end
 
 return workbench
