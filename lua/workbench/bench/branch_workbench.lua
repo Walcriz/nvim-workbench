a0b465cf0b72d3798ec8c874ce7eef74741023b8
Walcriz <walcriz.mail@gmail.com>
2023-03-11 10:21:13 +0100

    Now???

--- lua/workbench/bench/branch_workbench.lua
+++ lua/workbench/bench/branch_workbench.lua
@@ -2,12 +2,6 @@ local M = {}
 local utils = require("workbench.utils")
 local base_bench = require("workbench.bench")
 
-setmetatable(M, {
-	__call = function(_, ...)
-		return M.new(...)
-	end,
-})
-
 function M.new()
 	local self = setmetatable(base_bench:new(), { __index = M })
 	return self:init()
